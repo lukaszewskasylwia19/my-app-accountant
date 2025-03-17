ffa6a11a0520f0b78a6489fba9b0c05f47e8722b
lukaszewskasylwia19 <lukaszewskasylwia19@gmail.com>
2025-03-17 13:33:59 +0100

    Nadpisanie całego repozytorium nową wersją

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,3 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import "../../../style/layout/Layout.css"; // ✅ Poprawiona ścieżka
-
-
-
-interface LayoutProps {
-  children: React.ReactNode;
-  setSection: (section: string) => void;
-}
-
-export default function Layout({ children, setSection }: LayoutProps) {
-  return (
-    <div className="layout-container">
-      <Navbar setSection={setSection} />
-      <main className="layout-content">{children}</main>
-      <Footer />
-    </div>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,5 @@
 import "../../../style/layout/Navbar.css"; // ✅ Poprawiona ścieżka
+
 interface NavbarProps {
   setSection: (section: string) => void;
 }
